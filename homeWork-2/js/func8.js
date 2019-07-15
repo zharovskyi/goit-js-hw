1902db96da4525dc8269da94a8f0dec5aaac55df
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-07-15 14:21:37 +0300

    sasa

--- homeWork-2/js/func8.js
+++ homeWork-2/js/func8.js
@@ -1,21 +1,33 @@
 // // // Task 8
 // // спитати
-// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-// const isLoginValid = function (login) {
-//   if(login.includes)
-// };
-// // console.log(isLoginValid(logins));
+const isLoginValid = function (login) {
+  if(login.length > 3 && login.length < 16){
+      return true;
+  }
+  return false;
+};
+// console.log(isLoginValid('qwerty123'));
 
-// const isLoginUnique = function(allLogins, login) {
-//  if(allLogins.includes(login)){
-//    return true;
-//  }
-//  return false;
-// };
+const isLoginUnique = function(allLogins, login) {
+    return !allLogins.includes(login);
+};
 // console.log(isLoginUnique(logins, 'qwerty123'));
 
 
-// // const addLogin = function(allLogins, login) {
-
-// // };
+const addLogin = function(allLogins, login) {
+    if(isLoginValid(login)){
+        if (isLoginUnique(allLogins, login)){
+            allLogins.push(login);
+        alert('')
+        }
+    }else{
+        alert("Ошибка! Логин должен быть от 4 до 16 символов");
+    }
+    console.log(allLogins);
+};
+addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'jqueryisextremelyfast');
