594238099c409ba2568456e88b3327c78c2a993f
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-07-16 10:54:17 +0300

    add task

--- hw-4/js/task2-funThis.js
+++ hw-4/js/task2-funThis.js
@@ -2,26 +2,27 @@
 // чтобы скрипт заработал.
 
 const inventory = {
-    items: ['Монорельса', 'Фильтр'],
-    add(itemName) {
-      this.items.push(itemName);
-    },
-    remove(itemName) {
-        this.items = filter(item => item !== itemName);
-    }
-  };
-  
-  const invokeInventoryOperation = function(itemName, inventoryAction) {
-    console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
-    inventoryAction(itemName);
-  };
-  
-  invokeInventoryOperation('Аптечка', inventory.add);
-  // Invoking add opeartion on Аптечка
-  
-  console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
-  
-  invokeInventoryOperation('Фильтр', inventory.remove);
-  // Invoking remove opeartion on Фильтр
-  
-  console.log(inventory.items); // ['Монорельса', 'Аптечка']
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryOperation = function(itemName, inventoryAction) {
+  console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+  inventoryAction(itemName);
+};
+
+invokeInventoryOperation('Аптечка', inventory.add.bind(inventory));
+// Invoking add opeartion on Аптечка
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryOperation('Фильтр', inventory.remove.bind(inventory));
+// Invoking remove opeartion on Фильтр
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
+
