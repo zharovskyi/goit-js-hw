8dfb4972e5be0d49e77061962da767fe6faf25d1
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-07-16 09:48:25 +0300

    add 4

---
+++ hw-4/js/task2-funThis.js
@@ -0,0 +1,27 @@
+// Исправь ошибки которые будут в консоли, 
+// чтобы скрипт заработал.
+
+const inventory = {
+    items: ['Монорельса', 'Фильтр'],
+    add(itemName) {
+      this.items.push(itemName);
+    },
+    remove(itemName) {
+        this.items = filter(item => item !== itemName);
+    }
+  };
+  
+  const invokeInventoryOperation = function(itemName, inventoryAction) {
+    console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+    inventoryAction(itemName);
+  };
+  
+  invokeInventoryOperation('Аптечка', inventory.add);
+  // Invoking add opeartion on Аптечка
+  
+  console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+  
+  invokeInventoryOperation('Фильтр', inventory.remove);
+  // Invoking remove opeartion on Фильтр
+  
+  console.log(inventory.items); // ['Монорельса', 'Аптечка']
