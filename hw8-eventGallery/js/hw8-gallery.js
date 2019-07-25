886169ba59bc89c543b3d1fcd44bce96d45e86b6
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-07-25 19:28:33 +0300

    add image

--- hw8-eventGallery/js/hw8-gallery.js
+++ hw8-eventGallery/js/hw8-gallery.js
@@ -1,1 +1,19 @@
-// import ...
+import image from './gallery-items.js'
+console.log(image);
+
+let ul = document.querySelector('.gallery');
+function createGallery(image){  
+    let fragment = document.createDocumentFragment(); 
+    image.forEach(element => {
+        let li = document.createElement('li');
+        let prevImage = document.createElement('img');
+        prevImage.src = element.preview;
+        prevImage.className = 'image';
+        li.append(prevImage);
+        fragment.append(li);
+        console.log(li);
+     });
+     ul.prepend(fragment);
+     console.log(ul);
+ }
+ createGallery(image);
