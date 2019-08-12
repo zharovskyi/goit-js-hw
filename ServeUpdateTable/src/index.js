4fc7ce9c1a44baaac39cecbce670dd3a473a4a76
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-08-12 17:47:22 +0300

    add update

--- ServeUpdateTable/src/index.js
+++ ServeUpdateTable/src/index.js
@@ -1,1 +1,42 @@
 import './styles.css';
+
+const refs = {
+    button: document.querySelector('.button'),
+    table: document.querySelector('#table'),
+    baseUrl: 'http://localhost:3000/table',
+    th: document.querySelectorAll('th'),
+    td: document.querySelectorAll('td'),
+    tr: document.querySelectorAll('tr'),
+ 
+}
+
+refs.button.addEventListener('click', updateTable);
+
+
+function updateTable(e) {
+    e.preventDefault();
+    fetch(refs.baseUrl)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            data.headers.forEach((element, index) => {
+                // console.log(element);
+                refs.th[index].textContent = element;
+            })
+            data.data.forEach((elem,ind) => {
+                // console.log(elem);
+                // console.log(ind);
+                elem.forEach((e,i) => {
+                    refs.tr[ind + 1].querySelectorAll('td');
+                    console.log(refs.tr[ind + 1].querySelectorAll('td'));
+                    // for(let td of refs.td){
+                        // refs.td[i].textContent = e;
+                        
+                      
+                    // }
+                    
+                })
+
+            })
+        })
+}
